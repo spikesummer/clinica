b9e586b6b5c847be957c066e8822e2e629f1feda
Adriano Costa <spike.summer@hotmail.com>
2023-11-10 22:28:48 -0300

    atualização do Menu

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,78 @@
 import React from "react";
+import './style.css';
 
 const Home = props =>{
     return(
         <>
             {(sessionStorage.getItem("email") === "adriano@email.com" && sessionStorage.getItem("senha") === "123456") || (sessionStorage.getItem("email") === "professor@email.com" && sessionStorage.getItem("senha") === "123456")  ? 
-                <>
-                    <h1>Seja bem vindo</h1>
-                    
-                </>
+                <div className="container-fluid container-home p-0 m-0">
+                    <div className="container-fluid row p-0 m-0 container-home">
+                        {/* MENU */}
+                        <div className="col-md-3 col-lg-3 col-3 col-xl-2 bg-light menu">
+                            <div className="row justify-content-center">
+                                <div className="col text-center mt-5">
+                                    <img src="/apple-icon-180x180.png" class="img-fluid col-6" alt="icone"/>
+                                    <h2 className="mt-3 text-light">Whitetooth</h2>
+                                </div>
+                            </div>
+                            <div className="row mt-5 text-light">
+                                <div class="vstack gap-2">
+                                    <div class="p-2 text-light fs-4 hover">
+                                        <a href="/home">
+                                            <i className="bi bi-house me-2 text-light"/>
+                                            Inicio
+                                        </a>
+                                    </div>
+
+                                    <div class="p-2  fs-4 text-light hover">
+                                        <a href="/agenda">
+                                            <i class="bi bi-calendar3 me-2"></i>
+                                            Agendar
+                                        </a>
+                                    </div>
+                                        
+                                    <div class="p-2  fs-4 text-light hover">
+                                        <a href="/paciente">
+                                            <i class="bi bi-people me-2"></i>                                    
+                                            Pacientes
+                                        </a>
+                                    </div>                                
+
+                                    <div class="p-2  fs-4 text-light hover">
+                                        <i class="bi bi-graph-up-arrow me-2"></i>
+                                        Relatórios
+                                    </div>                                        
+                                        <div class="vstack gap-1">
+                                            <div class="p-2 hover ps-4">Consultas realizadas</div>
+                                            <div class="p-2 hover ps-4">Novos pacientes</div>
+                                            <div class="p-2 hover ps-4">Orçamentos</div>
+                                        </div>
+                                     
+                                        
+                                    <div class="p-2  fs-4 text-light hover">
+                                        <i class="bi bi-cash-coin me-2"></i>
+                                        Financeiro
+                                    </div>
+                                        <div class="vstack gap-1">
+                                            <div class="p-2 hover ps-4">Contas a Receber</div>
+                                            <div class="p-2 hover ps-4">Contas a Pagar</div>
+                                        </div>
+
+                                    <div class="p-2  fs-4 text-light hover">
+                                        <i class="bi bi-gear me-2"></i>
+                                        Configurações
+                                    </div>
+                                        <div class="vstack gap-1 hover">
+                                            <div class="p-2 ps-4">Cadastrar Funcionário</div>
+                                        </div>
+
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div className="col"><h1>main</h1></div>
+                    </div>                    
+                </div>
             : window.location.href = "/"}
         </>
     )
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import '../src/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
